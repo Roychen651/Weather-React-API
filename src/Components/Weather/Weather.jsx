bfa3609d57f3a23074970b90e79efeef8835994f
Roy Chen <roy97chen@gmail.com>
2024-08-03 10:02:04 +0300

    finish weather UI

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './Weather.css'
+import searchIMG from '../../assets/search.png'
+import { images } from '../../assets/images'; 
 
 const Weather = () => {
   return (
-    <div >
-        <h1>Weather</h1>
+    <div className='weather'>
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <img src={searchIMG} alt="search icon" />
+      </div>
+        <img src={images.clear} alt="" className='weather-icon' />
+        <p className='temperature'>16°C</p>
+        <p className='location'>London</p>
+        <div className='weather-data'>
+          <div className="col">
+            <img src={images.humidity} alt="" />
+            <div>
+              <p>91 %</p>
+              <span>Humidity</span>
+            </div>
+          </div>
+          <div className="col">
+            <img src={images.wind} alt="" />
+            <div>
+              <p>3.6 Km/h</p>
+              <span>Wind Speed</span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
